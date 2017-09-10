eb470f0b5ecfa511dd5b526b627d288a62f8c08a
mingsterism <mingsterism@users.noreply.github.com>
2017-09-10 12:32:03 +0800

    Add .next build files

---
+++ .next/dist/components/MyLayout.js
@@ -0,0 +1,40 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _Header = require('./Header');
+
+var _Header2 = _interopRequireDefault(_Header);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = 'C:\\Users\\mingk\\coding1\\fullstack\\projects\\coder-lands1\\components\\MyLayout.js';
+
+
+var layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DDD'
+};
+
+var Layout = function Layout(props) {
+  return _react2.default.createElement('div', { style: layoutStyle, __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    }
+  }, _react2.default.createElement(_Header2.default, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    }
+  }), props.children);
+};
+
+exports.default = Layout;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHNcXE15TGF5b3V0LmpzIl0sIm5hbWVzIjpbIkhlYWRlciIsImxheW91dFN0eWxlIiwibWFyZ2luIiwicGFkZGluZyIsImJvcmRlciIsIkxheW91dCIsInByb3BzIiwiY2hpbGRyZW4iXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7QUFBQSxBQUFPOzs7Ozs7Ozs7QUFFUCxJQUFNO1VBQWMsQUFDVixBQUNSO1dBRmtCLEFBRVQsQUFDVDtVQUhGLEFBQW9CLEFBR1Y7QUFIVSxBQUNsQjs7QUFLRixJQUFNLFNBQVMsU0FBVCxBQUFTLE9BQUEsQUFBQyxPQUFVLEFBQ3hCO3lCQUNFLGNBQUEsU0FBSyxPQUFMLEFBQVk7Z0JBQVo7a0JBQUEsQUFDRTtBQURGO0dBQUEsa0JBQ0UsQUFBQzs7Z0JBQUQ7a0JBREYsQUFDRSxBQUNDO0FBREQ7QUFBQSxZQUZKLEFBQ0UsQUFFUyxBQUdaO0FBUEQsQUFTQTs7a0JBQUEsQUFBZSIsImZpbGUiOiJNeUxheW91dC5qcyIsInNvdXJjZVJvb3QiOiJDOi9Vc2Vycy9taW5nay9jb2RpbmcxL2Z1bGxzdGFjay9wcm9qZWN0cy9jb2Rlci1sYW5kczEifQ==
